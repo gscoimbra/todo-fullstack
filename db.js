9d45afbce0db4565e1af85e98b224ef997b01f4b
Gabriel <gcoimbrabr03@gmail.com>
2025-05-27 23:26:45 -0300

    correção

--- db.js
+++ db.js
@@ -1,16 +1,11 @@
-// SEM HOSPEDAGEM(RODANDO LOCALMENTE NO MEU PGADMIN):
-// require('dotenv').config();
-// const { Pool } = require('pg');
-// const pool = new Pool();
-// module.exports = pool;
-
-// HOSPEDANDO NO RENDER:
 require('dotenv').config();
 const { Pool } = require('pg');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // Render exige SSL!
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
